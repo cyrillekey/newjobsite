0303a158b2878a20abe602c93462a454ef38652b
cyrille <cyrilleotieno7@gmail.com>
2021-09-06 00:10:38 +0000

    hello

--- displayjob/views.py
+++ displayjob/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from job.models import Job,Job_skill
 # Create your views here.
@@ -6,9 +6,15 @@ def home(request):
     job=Job.objects.all()[0:4]
     return render(request,'displayjob/html/home.html',{'jobs':job})
 def jobinfo(request,username):
-    job=Job.objects.all().filter(slug=username)
-    name=username.replace("-",' ')
-    name=name+"."
-    skill=Job_skill.objects.all().filter()
-    return render(request,'displayjob/html/jobinfo.html',{'username':job,'skills':skill})
+    user=request.user
+    if user.is_authenticated:
+        job=Job.objects.all().filter(slug=username)
+        name=username.replace("-",' ')
+        name=name+"."
+        skill=Job_skill.objects.all().filter()
+        return render(request,'displayjob/html/jobinfo.html',{'username':job,'skills':skill})
+        
+    else:
+        return redirect('login')
+    
 
